47ba09fcd5bb768b844e8583a453e1623d163ff9
Brian Klein <brian@Scipio-Linux.(none)>
2011-02-21 13:16:21 -0800

    This is the first commit

--- chatskiServer.cpp
+++ chatskiServer.cpp
@@ -1,3 +1,8 @@
 // Chatski Server
 // more changes
-#include <ncurses.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
