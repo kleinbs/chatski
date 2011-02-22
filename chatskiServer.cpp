8a98a1e2da4a854f129d628cf036a23894bf33b1
Brian Klein <brian@Scipio-Linux.(none)>
2011-02-21 16:48:53 -0800

    newest version

--- chatskiServer.cpp
+++ chatskiServer.cpp
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 #define MAXPENDING 5;
-#define SERVSOCK 90;
+#define SERVERPORT 90;
 
 void DieWithError(char *errorMessage);
 
@@ -16,9 +16,30 @@ int sockArr[10];
 
 int main(int argc, char *argv[])
 {
+
+ int servSock;
+ int clntSock;
+ struct sockaddr_in echoServAddr;
+ struct sockaddr_in echoClntAddr;
+ unsigned int clntLen;
+
+//assign the servert socket
+ if((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	dieWithError("socket() failed");
+
+//Create the local address structure
+memset(&echoServAddr, 0, sizeof(echoServAddr));
+echoServAddr.sin_family = AF_INET;
+echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+echoServAddr.sin_port = htons(echoServPort);
+
+if(bind(servSock, (struct sockaddr *), &echoServAddr, sizeOf(echoServAddr)) < 0)
+	dieWithError("bind() failed");
+
  for(;;)
  {
-  if(listen(SERVSOCK, MAXPENDING) < 0)
+  if(listen(servSock, MAXPENDING) < 0)
 	dieWithError("listen() failed");
- }	
-}
+ }
+
+}	
