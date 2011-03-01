e962dd186057e9c7707ed8c2753adafb9754c299
kleinbs <kleinbs1234@gmail.com>
2011-03-01 13:38:34 -0800

    this is new

--- comm.c
+++ comm.c
@@ -6,10 +6,14 @@
 void DieWithError( char *errorMessage );
 
 void comm(int sockfd) {
+    
     if( listen( sockfd, MAXPENDING ) < 0 )
-		DieWithError( "listen() failed on socket " + sockfd);
+		fprintf(stderr, "socket() failed on %i\n", sockfd);
     
-    
+   while(1)
+    {
+      //printf("i'm working");
+    }
     
     exit(1);
 } 
