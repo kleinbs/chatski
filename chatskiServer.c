d7e0f6d912e29b3d59ece7812e5cf20a6b534c62
kleinbs <kleinbs1234@gmail.com>
2011-03-06 17:57:38 -0800

    hi

--- chatskiServer.c
+++ chatskiServer.c
@@ -5,8 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "pipeStructs.c"
 
 #define MAXPENDING 5
+
 
 void DieWithError( char *errorMessage );
 void comm( int sockfd );
@@ -19,35 +21,58 @@ int main( int argc, char *argv[] ) {
 	int clntSock;
 	int pid;
 	int count = 0;
+	int thread_server[2], server_thread[2];
 	struct sockaddr_in echoServAddr;
 	struct sockaddr_in echoClntAddr;
+	struct pipes t;
+	struct sock_pipe c;
 	unsigned short echoServPort = 99;
 	unsigned int clntLen;
 	pthread_t thread;
-	int  iret, msgHandler;
+	int  iret, talk, msgHandler;
 	
 	printf("I am running!!\n");
 	/* Create socket for incoming connections */
 	if( ( servSock = socket( PF_INET, SOCK_STREAM, IPPROTO_TCP ) ) < 0 )
 		DieWithError( "socket() failed" );
+	
+	printf("created server socket\n");
+	
+	/* Create the needed pipes */
+	pipe(thread_server);
+	pipe(server_thread);
+	
+	t.listenPipe = thread_server[1];
+	t.talkPipe = server_thread[0];
+	
+	/* Create the talk thread to listen for messages */
+	talk = pthread_create( &thread, NULL, talk, (void*) &t);
+	
+	//printf("created talk thread\n");
 
 	/* Construct local address structure */
 	memset( &echoServAddr, 0, sizeof( echoServAddr ) );
 	echoServAddr.sin_family = AF_INET;
 	echoServAddr.sin_addr.s_addr = htonl( INADDR_ANY );
 	echoServAddr.sin_port = htons( echoServPort );
+	
+	printf("work to here");
 
 	/* Bind to the local address */
 	if( bind( servSock, ( struct sockaddr * ) &echoServAddr, sizeof( echoServAddr ) ) < 0 )
 		DieWithError( "bind() failed" );
-
+	
 	/* Mark the socket so it will listen for incoming connections */
 	if( listen( servSock, MAXPENDING ) < 0 )
 		DieWithError( "listen() failed" );
+	
+	printf("got to for loop\n");
 
 	for( ;; ) {
 		/* Set the size of the in-out parameter */
 		clntLen = sizeof( echoClntAddr );
+		
+		printf("work to here 1\n");
 		
 		/* Wait for a client to connect */
 		if( ( clntSock = accept( servSock, ( struct sockaddr * ) &echoClntAddr, &clntLen ) ) < 0 )
@@ -56,11 +81,15 @@ int main( int argc, char *argv[] ) {
 		/* clntSock is connected to a client */
 		printf( "Handling client %s\n", inet_ntoa( echoClntAddr.sin_addr ) );
 		
-		iret = pthread_create( &thread, NULL, comm, (void*) clntSock);
+		c.sock = clntSock;
+		c.listenPipe = server_thread[1];
+		c.talkPipe = thread_server[0];
+		printf("work to here\n");
+		iret = pthread_create( &thread, NULL, comm, (void*) &c);
 		threads[count] = iret;
 		count++;
 		//close(clntSock);
-		
+		printf("am i still working?\n");
 	}
 	/* NOT REACHED */
 }
--- comm.c
+++ comm.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "pipeStructs.c"
 
 #define MAXPENDING 5
 
 void DieWithError( char *errorMessage );
 
-void comm(int sock) {
+void comm(void* p) {
   
-   int n;
+   int n, nbytes;;
    char buffer[256];
-      
    bzero(buffer,256);
    
-   printf("establishing connection on socket %d\n", sock);
+   struct sock_pipe foo = *((struct sock_pipe*)(p)); 
+   
+   printf("establishing connection on socket %d\n", foo.sock);
     
    while(1)
     {
-     n = read(sock,buffer,255);
+     n = read(foo.sock,buffer,255);
      if (n < 0) 
        printf("ERROR reading from socket");
-     printf("Here is the message on socket %d: %s\n", sock, buffer);
-     n = write(sock,"I got your message",18);
+     write(foo.talkPipe, foo.sock, sizeof(buffer));
+     nbytes = read(foo.listenPipe, buffer, sizeof(buffer));
+     printf("Here is the message on socket %d: %s\n", buffer, foo.sock);
+     n = write(nbytes,"I got your message",18);
      if (n < 0) 
        printf("ERROR writing to socket");
     }
